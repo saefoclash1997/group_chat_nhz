4b6a27ae93829fdede59f7be182a2c1785a2b4ae
saefoclash1997 <saefoclash1@gmail.com>
2025-09-11 17:43:01 +0300

    Update pageview_welcome.dart

--- lib/screens/pageview_welcome.dart
+++ lib/screens/pageview_welcome.dart
@@ -8,9 +8,9 @@ class PageviewWelcome extends StatelessWidget {
     return Scaffold(
       body: PageView(
         children: [
-          Center(child: Text("welcome",style: TextStyle(color: Colors.red),),),
-          Center(child: Text("welcome",style: TextStyle(color: Colors.green),),),
-          Center(child: Text("welcome",style: TextStyle(color: Colors.blue),),),
+          Center(child: Text("Welcome to NHZ Chat App!",style: TextStyle(color: Colors.red),),),
+          Center(child: Text("In this App you can Chat With Your Friends",style: TextStyle(color: Colors.green),),),
+          Center(child: Text("Let's Get Started",style: TextStyle(color: Colors.blue),),),
 
 
 
